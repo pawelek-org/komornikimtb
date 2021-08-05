761fcef3d76fb94151dd3427cdf6b8e7b6b22ebf
pawelek-org <github@pawelek.e4ward.com>
2021-08-05 11:37:21 +0200

    commit

--- _scripts/strava_api.sh
+++ _scripts/strava_api.sh
@@ -4,4 +4,11 @@
 #set -o pipefail
 
 bundle exec rake strava:clubrides --trace
+STRAVA_FILE="./_pages/jezdzimy.md"
+dt=$(date '+%Y-%m-%d %H:%M:%S');
 
+if [ -f $STRAVA_FILE ]; then
+  git add $STRAVA_FILE
+  git commit -m "Strava API update: $dt"
+  git push
+fi
